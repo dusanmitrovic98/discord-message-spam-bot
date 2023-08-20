2f2f5a14bed34b3623ae48f8ddd8c30bcf93add1
Dušan Mitrović (Dusan Mitrovic) <62337583+dusanmitrovic98@users.noreply.github.com>
2023-08-20 23:22:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,47 @@ random_messages = [
     "Your potential is limitless!",
     "Spread kindness and positivity.",
     "Believe in the power of positivity.",
+    "You're a true inspiration.",
+    "Keep smiling and keep shining.",
+    "You're making progress every day.",
+    "You bring joy to those around you.",
+    "Keep pushing through challenges.",
+    "You're meant for great things!",
+    "Your positivity is contagious.",
+    "Take pride in your accomplishments.",
+    "You're a beacon of hope.",
+    "Keep your heart open to new possibilities.",
+    "You're changing lives with your actions.",
+    "Stay true to yourself and your values.",
+    "You're capable of overcoming any obstacle.",
+    "Your positivity brightens the world.",
+    "Keep chasing your dreams!",
+    "You're making a positive impact.",
+    "Your journey is an inspiring one.",
+    "Keep spreading love and positivity.",
+    "You're an amazing individual.",
+    "Believe in the beauty of each moment.",
+    "You're a source of strength for others.",
+    "Stay focused and keep moving forward.",
+    "You're making a difference one step at a time.",
+    "Your presence brings positivity to the room.",
+]
+
+intents = discord.Intents.default()  # Create a default intents object
+intents.typing = False  # You can customize these based on your bot's needs
+intents.presences = False
+
+client = discord.Client(intents=intents)  # Pass the intents object when creating the client
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+
+    while True:
+        random_message = random.choice(random_messages)
+        server = client.get_guild(SERVER_ID)
+        channel = server.get_channel(CHANNEL_ID)
+        await channel.send(random_message)
+        await asyncio.sleep(5)
+
+client.run(TOKEN)
